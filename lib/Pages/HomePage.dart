7744f64e689243673b682dfa7109c1d93f8ab6bd
c1337 <cyril199897@gmail.com>
2022-04-24 05:34:54 +0530

    abi and contract temp

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart';
 import 'package:web3dart/web3dart.dart';
 
@@ -15,6 +16,18 @@ class HoemPage extends StatefulWidget {
 class _HoemPageState extends State<HoemPage> {
   EtherAmount balance = new EtherAmount.zero();
   late Future<bool> loaded;
+
+  TextEditingController contractAddress = new TextEditingController();
+
+  Future<DeployedContract> loadContract(
+      {String contractAddress =
+          "0x45c7DC5c7CB32f989C64cE20269E714dcf0886f6"}) async {
+    final abi = await rootBundle.loadString("assets/Guide.json");
+    final contract = DeployedContract(ContractAbi.fromJson(abi, "Guide"),
+        EthereumAddress.fromHex(contractAddress));
+    return contract;
+  }
+
   Future<bool> connectTOChain() async {
     // start a client we can use to send transactions
     final client = Web3Client(widget.url, Client());
@@ -42,34 +55,56 @@ class _HoemPageState extends State<HoemPage> {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: FutureBuilder(
-              future: loaded,
-              builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Container(
-                    child: Text("waiting"),
-                  );
-                } else if (snapshot.hasError) {
-                  return Container(
-                    child: Text("Error in snapshot"),
-                  );
-                } else {
-                  return Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          child: Text("Balance"),
+          child: Column(
+            children: [
+              Container(
+                child: TextField(
+                  controller: contractAddress,
+                ),
+              ),
+              Container(
+                child: ElevatedButton(
+                  child: Text("add contract address"),
+                  onPressed: () {
+                    // loadContract(contractAddress.text);
+                    loadContract(
+                        contractAddress: contractAddress.text.length > 0
+                            ? contractAddress.text
+                            : "0x45c7DC5c7CB32f989C64cE20269E714dcf0886f6");
+                  },
+                ),
+              ),
+              FutureBuilder(
+                  future: loaded,
+                  builder:
+                      (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Container(
+                        child: Text("waiting"),
+                      );
+                    } else if (snapshot.hasError) {
+                      return Container(
+                        child: Text("Error in snapshot"),
+                      );
+                    } else {
+                      return Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              child: Text("Balance"),
+                            ),
+                            Container(
+                              child: Text(
+                                  "Ether  :" + balance.getInEther.toString()),
+                            ),
+                          ],
                         ),
-                        Container(
-                          child:
-                              Text("Ether  :" + balance.getInEther.toString()),
-                        ),
-                      ],
-                    ),
-                  );
-                }
-              }),
+                      );
+                    }
+                  }),
+            ],
+          ),
         ),
       ),
     );
